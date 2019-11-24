c70bac4133d17dd963cf312940735a279fe2bc76
neal811220 <neal811220@gmail.com>
2019-11-24 14:37:49 +0800

    Firest commit

---
+++ SimpleLoginApp/SimpleLoginApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleLoginApp
+//
+//  Created by Neal on 2019/11/18.
+//  Copyright © 2019 neal812220. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
